4c417a5a010487bc122abf2316d89d7062989ef4
@you._.jin_2 <86221508+youjin2github@users.noreply.github.com>
2021-12-22 14:39:43 +0900

    Delete slack.py

--- slack.py
+++
@@ -1,6 +0,0 @@
-from slacker import Slacker
-
-slack = Slacker('xoxb-2200325338311-2215264887379-7AhtyJ7JF7Rx9rJWkZgnSvyR')
-
-# Send a message to #general channel
-slack.chat.post_message('#bitcoin', 'Hello fellow slackers!')
