e9678b408a2a56fd3fca416c05aeb7a57264343a
@you._.jin_2 <86221508+youjin2github@users.noreply.github.com>
2021-12-22 14:43:08 +0900

    Delete 목표가.py

--- 목표가.py
+++
@@ -1,9 +0,0 @@
-import time
-import pyupbit
-import datetime
-
-def get_target_price(DOGE, k):
-    df = pyupbit.get_ohlcv(DOGE, interval="day", count=2)
-    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
-    return target_price
-print(get_target_price)
