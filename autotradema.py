3ebfae1f1e2f506e3b517e4f9fa3d6fbcdf2b054
@you._.jin_2 <86221508+youjin2github@users.noreply.github.com>
2022-03-30 21:50:25 +0900

    Add files via upload

---
+++ autotradema.py
@@ -0,0 +1,69 @@
+import time
+import pyupbit
+import datetime
+
+access = "5x5Sgt34z4KrptP1H3ibG52gdl8xzsxukC13Di0B"        
+secret = "UVBPCvKBhUxSIvxARx1QGMaXNPJGD8vt3FIdKwU2"  
+
+ticker = "KRW-ETC"
+
+def get_target_price(ticker, k):
+    """변동성 돌파 전략으로 매수 목표가 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=2)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
+    return target_price
+
+def get_start_time(ticker):
+    """시작 시간 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
+    return start_time
+
+def get_ma15(ticker):
+    """15일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
+    ma15 = df['close'].rolling(15).mean().iloc[-1]
+    return ma15
+
+def get_balance(ticker):
+    """잔고 조회"""
+    balances = upbit.get_balances()
+    for b in balances:
+        if b['currency'] == ticker:
+            if b['balance'] is not None:
+                return float(b['balance'])
+            else:
+                return 0
+    return 0
+
+def get_current_price(ticker):
+    """현재가 조회"""
+    return pyupbit.get_orderbook(ticker)["orderbook_units"][0]["ask_price"]
+
+# 로그인
+upbit = pyupbit.Upbit(access, secret)
+print("autotrade start")
+
+# 자동매매 시작
+while True:
+    try:
+        now = datetime.datetime.now()
+        start_time = get_start_time("KRW-ETC")
+        end_time = start_time + datetime.timedelta(days=1)
+
+        if start_time < now < end_time - datetime.timedelta(seconds=10):
+            target_price = get_target_price("KRW-ETC", 0.5)
+            ma15 = get_ma15("KRW-ETC")
+            current_price = get_current_price("KRW-ETC")
+            if target_price < current_price and ma15 < current_price:
+                krw = get_balance("KRW")
+                if krw > 5000:
+                    upbit.buy_market_order("KRW-ETC", krw*0.9995)
+        else:
+            etc = get_balance("ETC")
+            if etc > 0.00008:
+                upbit.sell_market_order("KRW-ETC", etc*0.9995)
+        time.sleep(1)
+    except Exception as e:
+        print(e)
+        time.sleep(1)
